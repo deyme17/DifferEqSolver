195a025f4f062ebeaf65cc853cc6a5919f69c4eb
dmytro <dmytro19022006@gmail.com>
2025-09-16 20:03:37 +0300

    main window

--- gui/app_window.py
+++ gui/app_window.py
@@ -1,16 +1,31 @@
 import tkinter as tk
-from tkinter import ttk, messagebox
-import numpy as np
+from tkinter import Tk, ttk, messagebox
+import time
 
-from gui.input_frame import InputFrame
-from gui.results_frame import ResultsFrame
+from . import InputFrame, ResultsFrame
+from core import ODESolver, GraphPlotter
 from utils.method_register import ODEMethodRegistry
 
 class ODESolverApp:
     """Main application class"""
-
-    def __init__(self, root):
+    def __init__(self, 
+                 root: Tk, 
+                 input_frame: InputFrame, 
+                 result_frame: ResultsFrame,
+                 solver: ODESolver, 
+                 plotter: GraphPlotter
+                 ):
+        """
+        Initialize the main ODE solver application.
+        Args:
+            root: The main Tkinter root window.
+            input_frame: Frame handling user input for the ODE problem.
+            result_frame: Frame responsible for displaying numerical results and performance metrics.
+            solver: Numerical solver instance used for integrating differential equations.
+            plotter: Class responsible for rendering solution plots.
+        """
         self.root = root
+        self.solver = solver
         self.root.title("Розв'язання Диференціальних рівнянь")
         self.root.geometry("900x600")
 
@@ -36,8 +51,8 @@ class ODESolverApp:
             tab.columnconfigure(0, weight=1)
             tab.rowconfigure(0, weight=1)
 
-        self.input_frame = InputFrame(self.tab1, self.calculate, ODEMethodRegistry.get_method_choices())
-        self.results_frame = ResultsFrame(self.tab2)
+        self.input_frame = input_frame(self.tab1, self.calculate, ODEMethodRegistry.get_method_choices())
+        self.results_frame = result_frame(self.tab2, plotter)
 
     def _configure_style(self):
         style = ttk.Style()
@@ -49,4 +64,24 @@ class ODESolverApp:
         style.configure('Header.TLabel', font=('Helvetica', 11, 'bold'))
 
     def calculate(self):
-        ...
+        try:
+            equation = self.input_frame.get_equation()
+            params = self.input_frame.get_inputs()
+            y0, t0, t_end, eps, method = params.values()
+
+            # start timer
+            start = time.time()
+            ts, ys = self.solver.solve(
+                epsilon=eps,
+                method=method,
+                equation=equation,
+                y0=y0, t0=t0, t_end=t_end
+            )
+            exec_time = time.time() - start
+
+            # update results
+            self.results_frame.update_results(ts, ys, exec_time)
+            self.tab_control.select(self.tab2)
+
+        except Exception as e:
+            messagebox.showerror("Помилка", str(e))
