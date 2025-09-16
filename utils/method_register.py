27a46b9726e458275ba92a585bb1bb1373b541a7
dmytro <dmytro19022006@gmail.com>
2025-09-16 20:18:15 +0300

    typing

--- utils/method_register.py
+++ utils/method_register.py
@@ -1,19 +1,23 @@
+from typing import Optional
 from core import ODEMethodInterface
 
 class ODEMethodRegistry:
-    _methods = {}
+    _methods: dict[str, ODEMethodInterface]
 
     @classmethod
-    def register(cls, method_class):
+    def register(cls, method_class: ODEMethodInterface) -> ODEMethodInterface:
         if not issubclass(method_class, ODEMethodInterface):
-            raise TypeError("Method must inherit from ODESolver")
+            raise TypeError("Method must inherit from ODEMethodInterface")
         cls._methods[method_class.__name__.lower()] = method_class
         return method_class
 
     @classmethod
-    def get_method(cls, method_id):
+    def get_method(cls, method_id: str) -> Optional[ODEMethodInterface]:
         return cls._methods.get(method_id)
 
     @classmethod
-    def get_method_choices(cls):
-        return [(key, getattr(cls._methods[key], "display_name", key.title())) for key in cls._methods]
+    def get_method_choices(cls) -> list[tuple[str, str]]:
+        return [
+            (key, getattr(cls._methods[key], "display_name", key.title()))
+            for key in cls._methods
+        ]
