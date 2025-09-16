813aa6618a93870358a6f3ebb20dd1fb59f23078
dmytro <dmytro19022006@gmail.com>
2025-09-16 17:20:06 +0300

    method register

---
+++ utils/method_register.py
@@ -0,0 +1,19 @@
+from methods.slae_solver import ODESolver
+
+class ODEMethodRegistry:
+    _methods = {}
+
+    @classmethod
+    def register(cls, method_class):
+        if not issubclass(method_class, ODESolver):
+            raise TypeError("Method must inherit from ODESolver")
+        cls._methods[method_class.__name__.lower()] = method_class
+        return method_class
+
+    @classmethod
+    def get_method(cls, method_id):
+        return cls._methods.get(method_id)
+
+    @classmethod
+    def get_method_choices(cls):
+        return [(key, getattr(cls._methods[key], "display_name", key.title())) for key in cls._methods]
