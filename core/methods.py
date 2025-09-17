881d2d9de5b692afab3c0cfd0b11104b289451ab
dmytro <dmytro19022006@gmail.com>
2025-09-17 12:36:43 +0300

    adams - 4

--- core/methods.py
+++ core/methods.py
@@ -43,22 +43,38 @@ class RungeKuttaMethod(ODEMethodInterface):
 
 
 class AdamsMethod(ODEMethodInterface):
-    """Explicit Adams method"""
+    """Explicit Adams-Bashforth 4th order method"""
     display_name = "Метод Адамса"
-
     def __init__(self):
-        self.prev_f = None
+        self.reset()
     
     def reset(self):
         """Reset stored values for new calculation"""
         self.prev_f_values = []
+        self.step_count = 0
+        self.rk_method = RungeKuttaMethod()
     
     def step(self, f: Callable, t: float, y: float, h: float) -> float:
-        if self.prev_f is None:
-            self.prev_f = f(t, y)
-            return y + h * self.prev_f
-        else:
-            f_curr = f(t, y)
-            y_next = y + h/2 * (3*f_curr - self.prev_f)
-            self.prev_f = f_curr
-            return y_next
+        current_f = f(t, y)
+        
+        if self.step_count < 3:
+            self.prev_f_values.append(current_f)
+            self.step_count += 1
+            return self.rk_method.step(f, t, y, h)
+        
+        if len(self.prev_f_values) == 3:
+            self.prev_f_values.append(current_f)
+        
+        y_next = y + (h/24) * (
+            55 * self.prev_f_values[3] -
+            59 * self.prev_f_values[2] +
+            37 * self.prev_f_values[1] -
+            9 * self.prev_f_values[0]
+        )
+        self.prev_f_values[0] = self.prev_f_values[1]
+        self.prev_f_values[1] = self.prev_f_values[2] 
+        self.prev_f_values[2] = self.prev_f_values[3]
+        self.prev_f_values[3] = f(t + h, y_next)
+        
+        self.step_count += 1
+        return y_next
